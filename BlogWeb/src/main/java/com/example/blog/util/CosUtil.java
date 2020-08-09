0ca76a9bc23380a0a91f34e84d25c567ff5231cd
86God <60735574+86God@users.noreply.github.com>
2020-08-09 13:41:30 +0800

    Update CosUtil.java

--- BlogWeb/src/main/java/com/example/blog/util/CosUtil.java
+++ BlogWeb/src/main/java/com/example/blog/util/CosUtil.java
@@ -12,8 +12,8 @@ import java.io.File;
 
 public class CosUtil {
     // 1 初始化用户身份信息（secretId, secretKey）。
-    public static  final String secretId = "AKIDGJ5brqjCCv75mGRvLW1GE0JL7gyI2ExT";
-    public static  final String secretKey = "OQsEM9jSwA5hcKOBpENHSN3UgnaXNr66";
+    public static  final String secretId = "AKIDjGYJn5GVqIVSZLECG3cfWAICiywgeVYR";
+    public static  final String secretKey = "SfcVDgtqmVzDCKCp9E8nCLj9wUlJhpgG";
     public static  final COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
     // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
 // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
